61c62a78ae3feeee4f65b8f041acb412391e0e1f
vipul <vipuldamor@gmail.com>
2024-09-07 15:57:56 +0530

    more DSA Practice Updated

---
+++ app/src/main/java/com/developers1993/PalindromeNumber.kt
@@ -0,0 +1,59 @@
+package com.developers1993
+
+import java.util.Locale
+
+/***
+ * A palindrome is something that reads the same forward and backward
+ * (e.g., "121", "racecar", "madam").
+ * **/
+class PalindromeNumber {
+
+    //for integer
+    fun isPalindrome(number: Int) :Boolean{
+        if (number<0) return false
+
+        var original = number
+        var reversed = 0
+
+        while (original!=0){
+            val lastDigit = original%10
+            reversed = reversed * 10 + lastDigit
+            original /= 10
+        }
+        return number == reversed
+    }
+
+    //for string
+    fun isPalindrome(input:String):Boolean{
+        return  (input.isNotEmpty() && input.reversed() == input)
+    }
+
+    //other approach
+    fun isPalindromeStringAlternative(str: String): Boolean {
+        val lowerStr = str.lowercase()  // Convert to lowercase for case-insensitivity
+        var left = 0
+        var right = lowerStr.length - 1
+
+        while (left < right) {
+            if (lowerStr[left] != lowerStr[right]) {
+                return false  // If mismatch found, it's not a palindrome
+            }
+            left++
+            right--
+        }
+
+        return true  // If no mismatches, it's a palindrome
+    }
+
+}
+
+fun main() {
+    val palindromeNumber = PalindromeNumber()
+   val result =  palindromeNumber.isPalindrome(121)
+   val resultString =  palindromeNumber.isPalindrome("SMS")
+   val resultStringOther =  palindromeNumber.isPalindromeStringAlternative("seeyou")
+
+    println(result)
+    println(resultString)
+    println(resultStringOther)
+}
